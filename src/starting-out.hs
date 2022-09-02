3a9da13709a48b3d7d5248c49fdf0e8219d3b804
Alyssa <alyssa.burlton@gmail.com>
2022-09-02 21:10:58 +0100

    stack project to get IJ project working. Attempt some more exercises

--- src/starting-out.hs
+++ src/starting-out.hs
@@ -1,12 +1,30 @@
+{-
+ - Literal examples from the chapter
+ -}
 doubleMe x = x + x
 
 doubleUs x y = doubleMe x + doubleMe y
 
-doubleSmallNumber x = if x > 100
-then x
-  else x*2
+doubleSmallNumber x =
+  if x > 100
+    then x
+    else x * 2
 
+boomBangs xs = [if x < 10 then "BOOM!" else "BANG!" | x <- xs, odd x]
 
+length' xs = sum [1 | _ <- xs]
+
+removeNonUppercase st = [ c | c <- st, c `elem` ['A'..'Z']]
+
+{-
+ - Me mucking around
+ -}
+fizzBuzz :: [Int] -> [String]
+fizzBuzz xi = [if (x `mod` 15) == 0 then "FizzBuzz" else if (x `mod` 5) == 0 then "Buzz" else if (x `mod` 3) == 0 then "Fizz" else show x | x <- xi]
+
+{-
+ - Exercises
+ -}
 -- Find the penultimate element in list l
 penultimate l = last (init l)
 
@@ -24,26 +42,26 @@ isPalindrome l = reverse l == l
  -
  - For example: concat [[1,2,3],[3,4,5]] returns [1,2,3,3,4,5]
  -}
-duplicate xs = concat ([xs] ++ [xs])
+duplicate xs = concat ([[x, x] | x <- xs])
 
 {-
- - Imitate the functinality of zip
+ - Imitate the functionality of zip
  - The function "min x y" returns the lower of values x and y
  - For example "ziplike [1,2,3] ['a', 'b', 'c', 'd']" returns [(1,'a'), (2, 'b'), (3, 'c')]
  -}
-ziplike xs ys = undefined
+ziplike xs ys = [(xs!!i, ys!!i) | i <-[0..min (length xs - 1) (length ys - 1)]]
 
 -- Split a list l at element k into a tuple: The first part up to and including k, the second part after k
 -- For example "splitAtIndex 3 [1,1,1,2,2,2]" returns ([1,1,1],[2,2,2])
-splitAtIndex k l = undefined
+splitAtIndex k l = (take k l, drop k l)
 
 -- Drop the element at index k in list l
 -- For example "dropK 3 [0,0,0,1,0,0,0]" returns [0,0,0,0,0,0]
-dropK k l = undefined
+dropK k l = [l!!i | i <-[0..length l - 1], i /= k]
 
 -- Extract elements between ith and kth element in list l. Including i, but not k
 -- For example, "slice 3 6 [0,0,0,1,2,3,0,0,0]" returns [1,2,3]
-slice i k l = undefined
+slice i k l = [l!!n | n <-[0..length l - 1], n >= i, n < k]
 
 -- Insert element x in list l at index k
 -- For example, "insertElem 2 5 [0,0,0,0,0,0]" returns [0,0,0,0,0,2,0]
